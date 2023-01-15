13bcf1ba6f5d09117edc38b7f6080a7bf1f7e0d1
Jonas Liekens <jonas.liekens@roots.be>
2023-01-15 17:23:48 +0100

    refactor: Uses item classifier to grade item quality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,61 +1,51 @@
 package com.gildedrose;
 
+import java.util.List;
+
 class GildedRose {
-    Item[] items;
+    final Item[] items;
+    final ItemClassifier itemClassifier = new ItemClassifier(
+        List.of("Sulfuras, Hand of Ragnaros"),
+        List.of("Aged Brie")
+    );
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (final Item item : items) {
+            final ItemClass itemClass = itemClassifier.classifyItem(item);
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+            // Update sellIn
+            if (!ItemClass.LEGENDARY.equals(itemClass)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            // Update quality
+            if (ItemClass.COMMON.equals(itemClass) && item.quality > 0) {
+                final int degradingModifier = item.sellIn < 0 ? 2 : 1;
+                item.quality -= degradingModifier;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
+            if (ItemClass.BACKSTAGE_PASS.equals(itemClass)) {
+                if (item.sellIn < 0) {
+                    item.quality = 0;
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        if (item.sellIn <= 5) {
+                            item.quality += 3;
+                        } else if (item.sellIn <= 10) {
+                            item.quality += 2;
+                        } else {
+                            item.quality += 1;
+                        }
                     }
                 }
+            }
+
+            if (ItemClass.AGING_CHEESE.equals(itemClass) && item.quality < 50) {
+                item.quality += 1;
             }
         }
     }
