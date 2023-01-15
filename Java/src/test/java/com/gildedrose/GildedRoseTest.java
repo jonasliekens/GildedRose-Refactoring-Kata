d6dd0af5efc0486a5c37000d6a5ba66f1bed7cda
Jonas Liekens <jonas.liekens@roots.be>
2023-01-15 16:14:05 +0100

    refactor: Adds unit tests to cover basic requirements

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,143 @@
 package com.gildedrose;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void itemsDoNotDegradePastZero() {
+        // Given
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
+
+        // When
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        // Then
+        Assertions.assertThat(app.items[0]).satisfies(item -> {
+            Assertions.assertThat(item.name).isEqualTo("foo");
+            Assertions.assertThat(item.quality).isEqualTo(0);
+            Assertions.assertThat(item.sellIn).isEqualTo(-1);
+        });
     }
 
+    @Test
+    void OnlyAgedBrieIncreasesInQuality() {
+        // Given
+        Item[] items = new Item[]{new Item("Aged Brie", 1, 0)};
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        Assertions.assertThat(app.items[0]).satisfies(item -> {
+            Assertions.assertThat(item.name).isEqualTo("Aged Brie");
+            Assertions.assertThat(item.quality).isEqualTo(1);
+            Assertions.assertThat(item.sellIn).isEqualTo(0);
+        });
+    }
+
+    @Test
+    void ItemHasMaxQualityOf50() {
+        // Given
+        Item[] items = new Item[]{new Item("Aged Brie", 1, 50)};
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        Assertions.assertThat(app.items[0]).satisfies(item -> {
+            Assertions.assertThat(item.name).isEqualTo("Aged Brie");
+            Assertions.assertThat(item.quality).isEqualTo(50);
+            Assertions.assertThat(item.sellIn).isEqualTo(0);
+        });
+    }
+
+    @Test
+    void SulfurasDoesNotDegradeInQualityOrSellInValue() {
+        // Given
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 10, 80)};
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        Assertions.assertThat(app.items[0]).satisfies(item -> {
+            Assertions.assertThat(item.name).isEqualTo("Sulfuras, Hand of Ragnaros");
+            Assertions.assertThat(item.quality).isEqualTo(80);
+            Assertions.assertThat(item.sellIn).isEqualTo(10);
+        });
+    }
+
+    @Test
+    void BackstagePassesIncreaseInValue() {
+        // Given
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 15, 25)};
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        Assertions.assertThat(app.items[0]).satisfies(item -> {
+            Assertions.assertThat(item.name).isEqualTo("Backstage passes to a TAFKAL80ETC concert");
+            Assertions.assertThat(item.quality).isEqualTo(26);
+            Assertions.assertThat(item.sellIn).isEqualTo(14);
+        });
+    }
+
+    @Test
+    void BackstagePassesIncreaseTwiceAsFastInValue10DaysFromConcert() {
+        // Given
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 10, 25)};
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        Assertions.assertThat(app.items[0]).satisfies(item -> {
+            Assertions.assertThat(item.name).isEqualTo("Backstage passes to a TAFKAL80ETC concert");
+            Assertions.assertThat(item.quality).isEqualTo(27);
+            Assertions.assertThat(item.sellIn).isEqualTo(9);
+        });
+    }
+
+    @Test
+    void BackstagePassesIncreaseThriceAsFastInValue5DaysFromConcert() {
+        // Given
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 5, 25)};
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        Assertions.assertThat(app.items[0]).satisfies(item -> {
+            Assertions.assertThat(item.name).isEqualTo("Backstage passes to a TAFKAL80ETC concert");
+            Assertions.assertThat(item.quality).isEqualTo(28);
+            Assertions.assertThat(item.sellIn).isEqualTo(4);
+        });
+    }
+
+    @Test
+    void BackstagePassesHaveZeroQualityAfterConcertDate() {
+        // Given
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 0, 25)};
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        Assertions.assertThat(app.items[0]).satisfies(item -> {
+            Assertions.assertThat(item.name).isEqualTo("Backstage passes to a TAFKAL80ETC concert");
+            Assertions.assertThat(item.quality).isEqualTo(0);
+            Assertions.assertThat(item.sellIn).isEqualTo(-1);
+        });
+    }
 }
